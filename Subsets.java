7d175f075add6234ae810f0d9177f301f111ee0f
Durga0610 <05durgagurunathan@gmail.com>
2026-04-27 21:35:54 +0530

    Implement subsets method to generate all subsets

---
+++ Subsets.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Subsets{
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        for (int num : nums) {
+            int size = result.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> temp = new ArrayList<>(result.get(i));
+                temp.add(num);
+                result.add(temp);
+            }
+        }
+        return result;
+
+    }
+}
