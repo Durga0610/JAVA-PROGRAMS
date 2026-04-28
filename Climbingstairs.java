2d4beaeec76b77660f8ab0e57b0369ad677aa0c9
Durga0610 <05durgagurunathan@gmail.com>
2026-04-28 23:24:01 +0530

    Implement Climbing Stairs solution in Java

---
+++ Climbingstairs.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Climbingstairs{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        if(n==1){
+            System.out.println(1);
+        }
+        int[] dp=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        System.out.println(dp[n]);
+    }
+}
